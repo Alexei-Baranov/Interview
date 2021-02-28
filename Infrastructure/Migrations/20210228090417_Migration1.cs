06dc7e6bc13bfc93552a860996db2d6f18fa6ddd
Алексей <baranovas85@gmail.com>
2021-02-28 13:44:54 +0300

    update

---
+++ Infrastructure/Migrations/20210228090417_Migration1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace InterviewService.Infrastructure.Migrations
+{
+    public partial class Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Question = table.Column<string>(type: "text", nullable: true),
+                    AllOptionRequired = table.Column<bool>(type: "boolean", nullable: false),
+                    MaxSelectedOptionCount = table.Column<int>(type: "integer", nullable: false),
+                    InterviewStart = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    InterviewEnd = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    ResponsesCountForPublish = table.Column<int>(type: "integer", nullable: false),
+                    ResponsesIsPublic = table.Column<bool>(type: "boolean", nullable: false),
+                    IsPublished = table.Column<bool>(type: "boolean", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedUserId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interviews", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Options",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    InterviewId = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    AnswerCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Options", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Options_Interviews_InterviewId",
+                        column: x => x.InterviewId,
+                        principalTable: "Interviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    InterviewId = table.Column<int>(type: "integer", nullable: false),
+                    OptionId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Answers_Interviews_InterviewId",
+                        column: x => x.InterviewId,
+                        principalTable: "Interviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Answers_Options_OptionId",
+                        column: x => x.OptionId,
+                        principalTable: "Options",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_InterviewId",
+                table: "Answers",
+                column: "InterviewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_OptionId",
+                table: "Answers",
+                column: "OptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Options_InterviewId",
+                table: "Options",
+                column: "InterviewId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answers");
+
+            migrationBuilder.DropTable(
+                name: "Options");
+
+            migrationBuilder.DropTable(
+                name: "Interviews");
+        }
+    }
+}
